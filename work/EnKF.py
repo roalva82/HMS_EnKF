d50a4581b95271c8c3ed73bac159538ff4f8beb4
Rodolfo Alvarado Montero <rodolfo.alvaradomontero@gmail.com>
2025-07-16 22:41:00 -0400

    callback function - not working yet

--- work/EnKF.py
+++ work/EnKF.py
@@ -98,6 +98,7 @@ def enKF(forecast,obs_operator,observation, R):
     #R = np.cov(observation)
     num = np.dot(P, np.transpose(obs_operator))
     den = np.dot(obs_operator, num) + R
+    print('covariance\n', P)
     temp = np.linalg.inv(den)
     gain = np.dot(num, temp)
     A = forecast + np.dot(gain, (observation - np.dot(obs_operator, forecast)))
@@ -114,17 +115,65 @@ def read_observations(fileName):
     df = df[["station_id","FLOW"]]
     return df
 
+def update_block(match):
+    block = match.group(0)
+    subbasin_id = match.group(2)
+
+    values = storage_values.get(subbasin_id)
+    if not values:
+        return block  # leave unchanged if no values
+
+    # Update Soil Storage
+    if "Soil Storage" in values:
+        block = re.sub(
+            r"(Soil Storage:\s*)([^\s]+)",
+            lambda m: m.group(1) + str(values["Soil Storage"]),
+            block
+        )
+
+    # Update Groundwater Storage values
+    if "Groundwater Storage" in values:
+        gw_values = values["Groundwater Storage"]
+        
+        # Replacement function that consumes values in order
+        def replace_gw(m):
+            if replace_gw.index < len(gw_values):
+                val = gw_values[replace_gw.index]
+                replace_gw.index += 1
+                return m.group(1) + str(val)
+            else:
+                return m.group(0)  # no change if we run out of values
+
+        replace_gw.index = 0
+        block = re.sub(
+            r"(Groundwater Storage:\s*)([^\s]+)",
+            replace_gw,
+            block
+        )
+
+    return block
+
 #%%
 statesDataFrame = read_states(zip_state_files, fields)
 flowsDataFrame = read_flow(zip_xml_files)
 simulations = pd.concat([statesDataFrame, flowsDataFrame], ignore_index=True)
-simulations.to_csv("readStates.csv", header=True, index=False)  
+try:
+    simulations.to_csv("readStates.csv", header=True, index=False)
+except:
+    pass
 observations = read_observations('dataQ.nc')
+
 #%% example of EnKF
-exclude = ['Subbasin', 'Ensemble', 'Variable']
+exclude = ['Subbasin', 'Variable']
 simulation = simulations.loc[simulations['Subbasin']=='SAM01', [col for col in simulations.columns if col not in exclude]].to_numpy()
 observation = observations.loc[observations['station_id']=='SAM01','FLOW'].iloc[-1]
 forecast = simulation.astype(float)
+
+
+###example for Porto Ivinhema
+#simulation = simulations.loc[simulations['Subbasin']=='SIH01', [col for col in simulations.columns if col not in exclude]].to_numpy()
+#observation = observations.loc[observations['station_id']=='SIH04','FLOW'].iloc[-1]
+###example for Porto Ivinhema
 
 ensembles = simulation.shape[1]
 observation = np.repeat(observation, ensembles)
@@ -133,29 +182,57 @@ obs_operator = np.array([[0,0,1]])
 obs_variance = 1e-4
 #%% example of EnKF
 analysis = enKF(forecast,obs_operator,observation, obs_variance)
-print(forecast)
-print(observation)
-print(analysis)
-#df.to_csv("FLOW2.csv", header=True, index=False)  
-### STEP 1: READ ALL THE STATES FROM ZIP FILES - STATES AND FLOW 
-#read input.state
-#read simulation.xml
-#build extended array
-#PARA FINALES DE MAYO!
+'''
+print('forecast\n', forecast)
+print('obs\n', observation)
+print('analysis\n', analysis)
+'''
+mean_analysis = np.mean(analysis, axis=1)
+print('mean analysis', mean_analysis)
+
+test = simulations.loc[simulations['Subbasin']=='SAM01', [col for col in simulations.columns if col in exclude]].to_numpy()
+df = pd.DataFrame([mean_analysis], columns=test)
+print(df)
 
 
-### STEP 2: READ ALL THE OBSERVATIONS
-# read file dataIn.nc using xarray library
-#ds = xr.open_dataset('dataIn.nc')
-#Qobs = ds['Qobs'] 
+zip_path = 'ensemble_states_0.zip'
+file_to_extract = 'Input.state'
+extract_to = './updates/'
+
+with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+    if file_to_extract in zip_ref.namelist():
+        zip_ref.extract(file_to_extract, extract_to)
 
 
-### STEP 3: RUN THE ASSIMILATION USING ENKF
-# run the assimilation procedure
+####COMENTARIO PARA SERGIO: CONVERTIR mean_analysis A UN DICCIONARIO COMO EL SIGUIENTE:
+storage_values = {
+    "SIH02": {
+        "Soil Storage": 1.1,
+        "Groundwater Storage": [0.01, 0.02]
+    },
+    "SIH03": {
+        "Soil Storage": 2.2,
+        "Groundwater Storage": [0.03, 0.04]
+    }
+}
+
+with open(extract_to + file_to_extract, 'r') as file:
+    content = file.read()
+    pattern = r"(Subbasin:\s*(\w+)(?:.|\n)*?End:)"
+    updated_text = re.sub(pattern, update_block, content)
 
 
-### STEP 4: WRITE RESULTS BACK INTO TH BASIN.STATE FILE
+with open(extract_to + file_to_extract, 'w') as file:
+    file.write(updated_text)
+
+'''
+## WRITE STATES BACK TO THE TEXT
+# Pattern to extract each Subbasin block
+"
 
 
+# Apply replacements
+updated_text = re.sub(pattern, update_block, text)
 
-# %%
+print(updated_text)
+'''
